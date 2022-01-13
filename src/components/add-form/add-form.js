b490306637eff5813508ca03b8320aa914788c6e
ilyats <ilya.tsokota@gmail.com>
2022-01-13 09:52:10 +0200

    add style

--- src/components/add-form/add-form.js
+++ src/components/add-form/add-form.js
@@ -1,29 +1,36 @@
-import './add-form.scss';
+import style from "./add-form.scss";
 
-import { useStore } from '../../store';
-import { useState } from 'preact/hooks';
+import { useStore } from "../../store";
+import { useState } from "preact/hooks";
 
 const AddForm = () => {
     const store = useStore();
-    const [text, setText] = useState('');
+    const [text, setText] = useState("");
 
     const onInput = (e) => {
-        setText(e.target.value.trim()); 
+        setText(e.target.value);
     };
 
     const onSubmit = (e) => {
         e.preventDefault();
+        const trimText = text.trim();
 
-        if (text.length > 0) {
-            store.addTodo(text);
-            setText('');
+        if (trimText.length > 0) {
+            store.addTodo(trimText);
+            setText("");
         }
     };
 
+    console.log(style);
     return (
-        <div class="add-form">
+        <div class={style["add-form"]}>
             <form onSubmit={onSubmit}>
-                <input type="text" onInput={onInput} value={text} placeholder="Write text..." />
+                <input
+                    type="text"
+                    onInput={onInput}
+                    value={text}
+                    placeholder="Write text..."
+                />
                 <button type="submit">Add</button>
             </form>
         </div>
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,19 @@
-import './app.scss';
+import style from "./app.scss";
 
-import { Store } from '../../store';
-import AddForm from '../add-form';
-import TodoList from '../todo-list';
+import { Store } from "../../store";
+import AddForm from "../add-form";
+import TodoList from "../todo-list";
 
 const App = () => {
     return (
-        <Store>
-            <div class="todo">
-                <AddForm />
-                <TodoList />
-            </div>
-        </Store>
+        <div class={style.page}>
+            <Store>
+                <div class="todo">
+                    <AddForm />
+                    <TodoList />
+                </div>
+            </Store>
+        </div>
     );
 };
 
